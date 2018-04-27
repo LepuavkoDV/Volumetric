6f2e848261df93d5ca567806ce06de1d7f2fa331
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-04-28 00:45:08 +0300

    added github repo link

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container-fluid dashboard" id="app">
     <router-view/>
+    <div class="row about align-items-center">
+        <div class="col-md-4 offset-4">
+          <a href="https://github.com/LepuavkoDV/Volumetric" title="https://github.com/LepuavkoDV/Volumetric"><i class="fab fa-github"></i></a>
+        </div>
+    </div>
   </div>
 </template>
 
