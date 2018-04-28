3510b7f8498aece1aeda4531e01b36ef6e49f130
Dmitriy Lepuavko <lepuavkodv@gmail.com>
2018-04-28 23:45:09 +0300

    lang switcher special chars replaced with fontawesome icons

--- src/lang/LangSwitcher.vue
+++ src/lang/LangSwitcher.vue
@@ -17,6 +17,6 @@ export default {
 
 <template>
   <div class="float-right lang-switcher">
-    <a v-on:click.prevent="setLanguage('ru')" v-bind:class="currentLanguage('ru')">ru</a> &#9900; <a v-on:click.prevent="setLanguage('ua')" v-bind:class="currentLanguage('ua')">ua</a> &#9900; <a v-on:click.prevent="setLanguage('en')" v-bind:class="currentLanguage('en')">en</a>
+    <a v-on:click.prevent="setLanguage('ru')" v-bind:class="currentLanguage('ru')">ru</a> <i class="far fa-circle"></i> <a v-on:click.prevent="setLanguage('ua')" v-bind:class="currentLanguage('ua')">ua</a> <i class="far fa-circle"></i> <a v-on:click.prevent="setLanguage('en')" v-bind:class="currentLanguage('en')">en</a>
   </div>
 </template>
